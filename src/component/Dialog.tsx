402e82e943db372950004c65f58a6401f2b67067
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2022-05-16 01:04:54 +0900

    basic dialog almost set up

--- src/component/Dialog.tsx
+++ src/component/Dialog.tsx
@@ -1,40 +1,60 @@
 import styled from "styled-components"
 import { StyledComponent } from "../type"
 
-interface DialogProps extends StyledComponent {}
+
+type CornerPlacement = "top-left" | "top-right" | "bottom-left" | "bottom-right"
+
+interface DialogProps extends StyledComponent {
+
+	chain?: boolean
+	corner?: false | CornerPlacement
+}
 
 
 const RawDialog = (props: DialogProps) => {
 	return (
-		<div>
-			<text>[text here]</text>
+		<>
+		<div className={props.className}>
+			<p>[text here]</p>
+			<Corner />
 		</div>
+		</>
 	)
 }
 
 
 const Dialog = styled(RawDialog)`
-	/* width: 90%; */
+	position: relative;
+	width: 90%;
+	min-height: 100px;
+	padding: 10px;
 	background-color: white;
 	border-radius: 20px;
-	box-shadow: 0px 8px 4px 0px rgba(0,0,0,0.25);
+	box-shadow: 0px 6px 2px 0px rgba(0,0,0,0.25);
 `
 
 
-interface CornerProps extends StyledComponent {}
+interface CornerProps extends StyledComponent {
+
+}
 
 
 const RawCorner = (props: CornerProps) => {
 	return (
 		<div className={props.className}>
-
+			this is corner
 		</div>
 	)
 }
 
 
 const Corner = styled(RawCorner)`
-	
+	position: absolute;
+	left: calc(10%);
+	bottom: -100px;
+	width: 90px;
+	height: 100px;
+	background-color: red;
 `
 
 
--- src/component/DialogRegion.tsx
+++ src/component/DialogRegion.tsx
@@ -1,6 +1,7 @@
 
 import styled from "styled-components"
 import { StyledComponent } from "../type"
+import Dialog from "./Dialog"
 
 interface DialogRegionProps extends StyledComponent {}
 
@@ -8,14 +9,18 @@ interface DialogRegionProps extends StyledComponent {}
 const RawDialogRegion = (props: DialogRegionProps) => {
 	return (
 		<div className={props.className}>
-			<span>dialog region</span>
+			<Dialog />
 		</div>
 	)
 }
 
 
 const DialogRegion = styled(RawDialogRegion)`
+	padding: 10px 0px;
 	flex: 2 1 0;
+	display: flex;
+	flex-flow: column nowrap;
+	align-items: center;
 `
 
 
